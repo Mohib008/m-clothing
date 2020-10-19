58cc117bbe5db23d5dc5e5c9238ef4ea41bfef63
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-10-19 06:12:28 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Homepage from './Components/homepage';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello world! </h1>
+    <Header />
+     <Homepage />
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,64 @@
+import React, { classes } from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import { Avatar, IconButton, TextField } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+//import { useStateValue } from "./StateProvider";
+//import { Photo } from '@material-ui/icons';
+//import userEvent from '@testing-library/user-event';
+
+
+
+function Header() {
+    //const [{user}, dispatch] = useStateValue();
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQOPd4RlVof0ztjbcbcmcDjzfs1hQV6AL21BA&usqp=CAU"
+                     alt=""
+            />
+            </div>
+            
+            <div className="header__center">
+                <div className="header__option--active">
+                  <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" placeholder="Search Stor" />
+                </div>
+            </div>
+
+            <div className="header__right">
+                <form className= {classes} noValidate autoComplete="off" style={{ paddingRight:"10px"}}>
+                    <TextField id="outlined-basic" label="Search Field" variant="outlined" />
+                </form>
+                <div className="header__info">
+                    <Avatar />
+                    <h4>LogIn</h4>
+                </div>
+                <div className="header__icon">
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <AddShoppingCartIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

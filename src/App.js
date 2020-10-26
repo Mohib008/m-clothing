d482c941af14d8f2da2cfdf5d4e48b256ea9bad5
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-10-26 04:36:16 -0400

    app and index files are updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Pages/homepage';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './Pages/homepage';
 import Header from './Components/Header';
 
+
+
 function App() {
+
+const HatsPage = () => (
+  <div> <h1>Hats Page </h1></div>
+)
+
   return (
     <div className="App">
     <Header />
-     <Homepage />
+    <Switch>
+     <Route  path='/' component={ HomePage } />
+      <Route path='/hats' component={HatsPage} />
+    </Switch>
     </div>
   );
 }
